14bdc0aef6ffa87c7384c1a7ee46514f9c3d958a
nvmnghia <nvmnghia@gmail.com>
2021-12-28 15:48:37 +0700

    fix(chart): various fixes for Stop order form
    - Margin for inputs
    - Fix higher/lower radio

--- pages/placeorder/placeorder.js
+++ pages/placeorder/placeorder.js
@@ -1,7 +1,11 @@
-window.onload = () => {
+function preventReloadBuySell() {
     const forms = document.querySelectorAll('form');
     const prevent = event => event.preventDefault();
     forms.forEach(form => {
         form.addEventListener('submit', prevent);
     });
+}
+
+window.onload = () => {
+    preventReloadBuySell();
 }
