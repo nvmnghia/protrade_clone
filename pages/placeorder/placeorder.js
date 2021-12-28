a959c7867b9f2f4d0e185485d11ca1a88788de26
nvmnghia <nvmnghia@gmail.com>
2021-12-28 16:49:38 +0700

    feat(chart): toggle forms using radio

--- pages/placeorder/placeorder.js
+++ pages/placeorder/placeorder.js
@@ -6,6 +6,24 @@ function preventReloadBuySell() {
     });
 }
 
+/**
+ * Given a change event of a radio for a form, toggle that form.
+ *
+ * @param {Event} radioEvent Change event of the radio
+ */
+function toggleForm(radioEvent) {
+    const radio = radioEvent.target;
+    const formID = `${radio.id}-form`;
+
+    document.querySelectorAll(`form:not(#${formID})`)
+        .forEach(form => form.style.display = 'none');
+
+    document.getElementById(formID).style.display = 'block';
+}
+
 window.onload = () => {
     preventReloadBuySell();
+
+    document.getElementById('normal-order').onchange = toggleForm;
+    document.getElementById('stop-order').onchange = toggleForm;
 }
